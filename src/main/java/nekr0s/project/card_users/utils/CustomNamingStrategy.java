eff64e7038796bbc576dabc614a650b5ea034e74
nekr0s <alexanderrusev@abv.bg>
2018-10-27 23:37:30 +0300

    CustomNamingStrategy finished

--- src/main/java/nekr0s/project/card_users/utils/CustomNamingStrategy.java
+++ src/main/java/nekr0s/project/card_users/utils/CustomNamingStrategy.java
@@ -4,6 +4,7 @@ import org.hibernate.cfg.ImprovedNamingStrategy;
 
 public class CustomNamingStrategy extends ImprovedNamingStrategy {
     private static final String PLURAL_SUFFIX = "s";
+    private static final String CORNER_CASE = "ID";
 
     @Override
     public String classToTableName(String className) {
@@ -15,5 +16,19 @@ public class CustomNamingStrategy extends ImprovedNamingStrategy {
         StringBuilder sb = new StringBuilder();
         sb.append(tableNameInSingularForm).append(PLURAL_SUFFIX);
         return sb.toString();
+    }
+
+    @Override
+    public String columnName(String columnName) {
+        if (columnName.toUpperCase().endsWith(CORNER_CASE)) {
+            int lastCharIndex = columnName.length() - 1;
+            return firstLetterToUpper(columnName.substring(0, lastCharIndex) +
+                    Character.toUpperCase(columnName.charAt(lastCharIndex)));
+        }
+        return firstLetterToUpper(columnName);
+    }
+
+    private String firstLetterToUpper(String input) {
+        return input.substring(0, 1).toUpperCase() + input.substring(1);
     }
 }
--- src/test/java/nekr0s/project/card_users/CardUsersApplicationTests.java
+++ src/test/java/nekr0s/project/card_users/CardUsersApplicationTests.java
@@ -1,5 +1,7 @@
 package nekr0s.project.card_users;
 
+import nekr0s.project.card_users.utils.CustomNamingStrategy;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +12,25 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class CardUsersApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void CustomNamingStrategy_ShouldReturn_InCorrectNamingConvention() {
+        CustomNamingStrategy customNamingStrategy = new CustomNamingStrategy();
+
+        String input = "personalNumber";
+
+        String result = "PersonalNumber";
+
+        Assert.assertEquals(result, customNamingStrategy.columnName(input));
+    }
+
+    @Test
+    public void CustomNamingStrategy_ShouldCover_CornerCase() {
+        CustomNamingStrategy customNamingStrategy = new CustomNamingStrategy();
+
+        String input = "requestId";
+
+        String result = "RequestID";
+
+        Assert.assertEquals(result, customNamingStrategy.columnName(input));
     }
 
 }
